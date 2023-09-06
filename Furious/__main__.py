470c4618e038817955e00c44bea839368aa40dbb
Loren Eteval <loren.eteval@proton.me>
2023-09-07 00:15:05 +0800

    Add copyright notice
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- Furious/__main__.py
+++ Furious/__main__.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2023  Loren Eteval <loren.eteval@proton.me>
+#
+# This file is part of Furious.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from Furious.Widget.Application import Application
 from Furious.Widget.Widget import MessageBox
 from Furious.Utility.Constants import APPLICATION_NAME, CRASH_LOG_DIR
