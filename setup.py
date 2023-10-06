6c5fd44d498db97dc2a70b34c80aff39c5bf395f
Loren Eteval <loren.eteval@proton.me>
2023-10-06 22:38:15 +0800

    Update dependency version
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'PySide6-Essentials',
         'Xray-core',
-        'hysteria',
+        'hysteria > 1.3.5',
         'hysteria2 > 2.0.0',
         'tun2socks > 2.5.1',
         'ujson',
