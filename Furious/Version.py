3ce1e53bb4c7cba2da36c687e346cd74bb5982cc
Loren Eteval <loren.eteval@proton.me>
2023-10-15 15:08:55 +0800

    Update version to 0.2.10
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- Furious/Version.py
+++ Furious/Version.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = '0.2.9'
+__version__ = '0.2.10'
