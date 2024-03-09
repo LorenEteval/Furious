b71764515e64fb8f4af3bf585e5f214d86b71d7f
Loren Eteval <loren.eteval@proton.me>
2024-03-09 19:17:56 +0800

    Bump version to 0.3.9
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- Furious/Version.py
+++ Furious/Version.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = '0.3.8'
+__version__ = '0.3.9'
