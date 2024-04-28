77ac1567720337b0398a7fa55fc5a12df9f25429
Loren Eteval <loren.eteval@proton.me>
2024-04-28 20:02:41 +0800

    Refine indentation control
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- Furious/Widget/IndentSpinBox.py
+++ Furious/Widget/IndentSpinBox.py
@@ -42,21 +42,21 @@ class IndentSpinBox(AppQDialog):
 
         self.setWindowTitle(_('Set Indent'))
 
-        self.indentText = QLabel(_('Indent:'))
+        self.indentText = AppQLabel(_('Indent:'))
 
         self.indentSpin = QSpinBox()
         self.indentSpin.setRange(0, 8)
         self.indentSpin.setValue(2)
 
-        self.dialogBtns = QDialogButtonBox(QtCore.Qt.Orientation.Horizontal)
-
-        self.dialogBtns.addButton(_('OK'), QDialogButtonBox.ButtonRole.AcceptRole)
-        self.dialogBtns.addButton(_('Cancel'), QDialogButtonBox.ButtonRole.RejectRole)
+        self.dialogBtns = AppQDialogButtonBox(QtCore.Qt.Orientation.Horizontal)
+        self.dialogBtns.addButton(_('OK'), AppQDialogButtonBox.ButtonRole.AcceptRole)
+        self.dialogBtns.addButton(
+            _('Cancel'), AppQDialogButtonBox.ButtonRole.RejectRole
+        )
         self.dialogBtns.accepted.connect(self.accept)
         self.dialogBtns.rejected.connect(self.reject)
 
         layout = QFormLayout()
-
         layout.addRow(self.indentText, self.indentSpin)
         layout.addRow(self.dialogBtns)
         layout.setFormAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
@@ -65,11 +65,3 @@ class IndentSpinBox(AppQDialog):
 
     def value(self):
         return self.indentSpin.value()
-
-    def retranslate(self):
-        self.setWindowTitle(_(self.windowTitle()))
-
-        self.indentText.setText(_(self.indentText.text()))
-
-        for button in self.dialogBtns.buttons():
-            button.setText(_(button.text()))
