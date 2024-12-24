8a3d77f6e2a5f8c604897a9a0ede65b472d92baf
Loren Eteval <loren.eteval@proton.me>
2024-12-24 12:07:00 +0800

    Update application exitcode
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- Furious/Interface/Application.py
+++ Furious/Interface/Application.py
@@ -23,9 +23,9 @@ __all__ = ['ApplicationFactory']
 class ApplicationFactory:
     class ExitCode:
         ExitSuccess = 0
-        UnknownException = 1
-        PlatformNotSupported = 2
-        AssertionError = 3
+        UnknownException = 61
+        PlatformNotSupported = 62
+        AssertionError = 63
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
