db2b69c5aa331aea40aa7d8bf6fbca8344c2608e
LorenEteval <loren.eteval@proton.me>
2026-06-15 18:26:29 +0800

    Added missing "from __future__ import annotations" in project files
    Signed-off-by: LorenEteval <loren.eteval@proton.me>

--- Furious/Qt/AppStyleSheet.py
+++ Furious/Qt/AppStyleSheet.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 from textwrap import dedent
 
 __all__ = ['AppStyleSheet']
